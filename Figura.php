5aa27468e66d27d6034d79f9d28361947cc6490a
dudwcs <dudwcs@gmail.com>
2024-02-05 14:06:18 +0100

    init ejemplo básico herencia

---
+++ Figura.php
@@ -0,0 +1,19 @@
+<?php
+class Figura
+{
+    protected int $num_lados;
+    protected string $nombre;
+
+    
+
+    public function calcularArea()
+    {
+        echo "<p> Calculando el área de $this->nombre ...</p>";
+    }
+
+    public function __construct(string $nombre, int $num_lados = 0)
+    {
+        $this->nombre = $nombre;
+        $this->num_lados = $num_lados;
+    }
+}
